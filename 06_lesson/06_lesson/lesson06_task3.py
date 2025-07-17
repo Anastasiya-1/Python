2c16ca58197febddf177141a8f9bd04bbebdad70
Anastasiya-1 <89225720006g@gmail.com>
2025-07-17 09:22:08 +0500

    исправление ошибок

--- 06_lesson/06_lesson/lesson06_task3.py
+++ 06_lesson/06_lesson/lesson06_task3.py
@@ -1,6 +1,4 @@
-"""
-Дождаться картинки
-"""
+"""Дождаться загрузки всех картинок и вывести src третей."""
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,19 +7,28 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
 try:
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
-
     wait = WebDriverWait(driver, 15)
-    # Ждём, когда у третьей картинки появится src
-    all_imgs = wait.until(lambda d: d.find_elements(By.CSS_SELECTOR, "img"))
-    wait.until(
-        lambda d: (
-            all_imgs[2].get_attribute("src")
-            and not all_imgs[2].get_attribute("src").endswith("blank.png")
-        )
-    )
-    src = all_imgs[2].get_attribute("src")
+
+    # 1. Явно ждём, что на странице есть минимум 3 картинки
+    wait.until(lambda d: len(d.find_elements(By.CSS_SELECTOR, "img")) >= 3)
+
+    def third_img_loaded(d):
+        """
+        Проверяет, загружено ли третье изображение на странице
+        """
+        imgs = d.find_elements(By.CSS_SELECTOR, "img")
+        if len(imgs) < 3:
+            return False
+        src = imgs[2].get_attribute("src")
+        if src and not src.endswith("blank.png"):
+            return src
+        return False
+
+    # 2. Ждём, что третья картинка загружена (src действительный)
+    src = wait.until(third_img_loaded)
     print(src)
 finally:
     driver.quit()
