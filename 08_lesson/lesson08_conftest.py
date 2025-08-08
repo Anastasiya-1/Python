c4c93d8dc2ebdf1178d7c69347dfb20fb0abc866
Anastasiya-1 <89225720006g@gmail.com>
2025-08-08 11:21:27 +0500

    изменения 2

--- 08_lesson/lesson08_conftest.py
+++
@@ -1,5 +0,0 @@
-from yougile_api import YougileAPI
-
-def test_simple():
-    # Заменить на реальные значения перед запуском тестов!
-    api = YougileAPI("https://api.yougile.com", "token")
--- 08_lesson/yougile_api.py
+++
@@ -1,4 +0,0 @@
-class YougileAPI:
-    def __init__(self, base_url, token):
-        self.base_url = base_url
-        self.token = token
